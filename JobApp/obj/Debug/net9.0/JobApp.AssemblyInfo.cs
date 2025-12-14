f0fc8afbcc40014ae11f675176a4674d754e67ec
CivancanAktas <civancanaktas@gmail.com>
2025-12-15 00:47:07 +0300

    son kontrol

--- JobApp/obj/Debug/net9.0/JobApp.AssemblyInfo.cs
+++ JobApp/obj/Debug/net9.0/JobApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60fa4cd5a5885e809e3333b13ce729525fc8423e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f771e51eae061ad36f44ea5174f947dca5204124")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
